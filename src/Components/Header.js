cff069824417dde719ffa849c6ac7f8e0f050e52
Shankarprince <shankarsowmiya07@gmail.com>
2022-03-07 20:09:23 +0530

    committed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,37 @@
+import {useEffect, useState} from "react";
+
 export function Header() {
+
+
+    const [ navBarLinks, setNavBarLinks] = useState(true);
+    const listItems = { display : navBarLinks ? "block" : "none" };
+
+    const toggleButton = () => {
+        // setNavBarLinks(!navBarLinks);
+        setNavBarLinks( function(state) {
+            return !state;
+        })
+    }
+
+    useEffect( function() {
+        console.log("Current navbar " + navBarLinks);
+    },[navBarLinks])
+    
     return (
         <header className="header">
             <p className="header-name">Shankar</p>
-            <a href="/" className="toggle-button">
+            <a href="/" className="toggle-button" onClick={toggleButton}>
                 <span className="toggle-button-bar"></span>
                 <span className="toggle-button-bar"></span>
                 <span className="toggle-button-bar"></span>
             </a>
-            <nav className="header-nav">
-                <ul className="header-nav-links">
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#skills">Skills</a></li>
-                    <li><a href="#projects">Projects</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                </ul>
-            </nav>
+            <ul style={listItems}>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
         </header>
     );
 }
