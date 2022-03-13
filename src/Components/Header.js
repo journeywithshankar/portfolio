7f6f56f3740d6edde4837d27d168dddd105051c5
Shankarprince <shankarsowmiya07@gmail.com>
2022-03-13 15:56:57 +0530

    committed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,6 @@ import {useEffect, useState} from "react";
 
 export function Header() {
 
-
     const [ navBarLinks, setNavBarLinks] = useState(true);
     const listItems = { display : navBarLinks ? "block" : "none" };
 
@@ -20,15 +19,15 @@ export function Header() {
     return (
         <header className="header">
             <p className="header-name">Shankar</p>
-            <a href="/" className="toggle-button" onClick={toggleButton}>
+            <div className="toggle-button" onClick={toggleButton}>
                 <span className="toggle-button-bar"></span>
                 <span className="toggle-button-bar"></span>
                 <span className="toggle-button-bar"></span>
-            </a>
-            <ul style={listItems}>
+            </div>
+            <ul className="header-nav-links" style={listItems}>
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
-                <li><a href="#skills">Skills</a></li>
+                <li><a onClick={() => console.log("clicked")} href="#skills">Skills</a></li>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
