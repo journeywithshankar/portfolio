9ea5ab18c0a290c1f31ad1bdf4e7366f6ad81bcc
Shankar <89457569+Shankarprince@users.noreply.github.com>
2022-06-06 21:11:10 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,11 +6,11 @@ export function Home() {
                 <div className="home-text-container">
                     <div className="home-text">
                         <h1>Hey, I am a <span>Full Stack Developer</span></h1>
-                        <p>Hi, I am Shankar from Chennai. I help brands turn their ideas into high quality websites.</p>
+                        <p>This is Shankar. I help brands turn their ideas into high quality websites.</p>
                         <div className="home-links-container">
                             <a target="_blank" rel="noreferrer" href="https://github.com/Shankarprince" className="home-links">Github</a>
                             <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/shankarprince" className="home-links">LinkedIn</a>
-                            <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1EWK7jHCQpQqsrHkNiL0i6f1CD5jUtZWI/view?usp=sharing" className="home-links">Resume</a>
+                            <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1SvqBwH9IIrFA1FEZAvZjVvMxpi7hORbx/view?usp=sharing" className="home-links">Resume</a>
                         </div>
 
                     </div>
